25ca7f410dae6bb6c674b905953812a131b1b163
Astel Thottankara <57724542+astelThottankara@users.noreply.github.com>
2020-03-30 18:00:11 +0530

    Source code created

---
+++ Code Chef/chef-swaps-digits.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Test
+{
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+	        int t = s.nextInt();
+	        int a,b;
+	        while(t-->0)
+	        {
+	            a=s.nextInt();
+	            b = s.nextInt();
+	            if(a/10==0 || b/10==0)
+	                System.out.println(a+b);
+	            else if(a/10<b%10)
+	            {
+	                a = (b%10)*10+a%10+(b/10)*10+a/10;
+	                System.out.println(a);
+	            }
+	            else if(b/10<a%10)
+	            {
+	                a = (a%10)*10+b%10+(a/10)*10+b/10;
+	                System.out.println(a);
+	            }
+	            else
+	                System.out.println((a+b));
+	        }
+	}			
+}
