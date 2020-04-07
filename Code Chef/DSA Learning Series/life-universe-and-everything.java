07f175dcaa6efc7a2e4fca073156811ab2498d39
Astel Thottankara <57724542+astelThottankara@users.noreply.github.com>
2020-04-07 21:56:42 +0530

    Create life-universe-and-everything.java

---
+++ Code Chef/DSA Learning Series/life-universe-and-everything.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true)
+		{
+		    int temp = Integer.parseInt(br.readLine());
+		    if(temp == 42)
+		        break;
+		    System.out.println(temp);
+		}
+	}
+}
