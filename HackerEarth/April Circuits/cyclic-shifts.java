d6ec45bc93c044921a0ee83ae7ba03a3fb09802c
Astel Thottankara <57724542+astelThottankara@users.noreply.github.com>
2020-04-26 10:42:59 +0530

    Update cyclic-shifts.java

--- HackerEarth/April Circuits/cyclic-shifts.java
+++ HackerEarth/April Circuits/cyclic-shifts.java
@@ -1,1 +1,67 @@
-//will be uploaded soon after the contest
+import java.util.*;
+
+class TestClass 
+{
+    private static final long x = 16;
+    
+    static long left(long n, long d) 
+    { 
+        int[] bits = new int[16];
+        for(int i=0;i<16;i++)
+            if((n&(1<<i))!=0)
+                bits[i]=1;
+        if(d>=16)
+            d = d%16;
+        while(d-->0)
+        {
+            int temp=bits[15];
+            for(int i=14;i>=0;i--)
+                bits[i+1] = bits[i];
+            bits[0] = temp;
+        }
+        long newNum=0;
+        for(int i=0;i<16;i++)
+            if(bits[i]==1)
+                newNum |= (1<<i);
+        return newNum;
+    } 
+
+    static long right(long n, long d) 
+    { 
+        int[] bits = new int[16];
+        for(int i=0;i<16;i++)
+            if((n&(1<<i))!=0)
+                bits[i]=1;
+        if(d>=16)
+            d = d%16;
+        while(d-->0)
+        {
+            int temp=bits[0];
+            for(int i=0;i<15;i++)
+                bits[i] = bits[i+1];
+            bits[15] = temp;
+        }
+        long newNum=0;
+        for(int i=0;i<16;i++)
+            if(bits[i]==1)
+                newNum |= (1<<i);
+        return newNum;
+    } 
+
+    public static void main(String args[] ) throws Exception 
+    {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        while(t-->0)
+        {
+            long n = s.nextLong();
+            long d = s.nextLong();
+            char c = s.next().charAt(0);
+            if(c=='L')
+                System.out.println(left(n,d));
+            else if(c=='R')
+                System.out.println(right(n,d));
+        }
+    }
+}
+
