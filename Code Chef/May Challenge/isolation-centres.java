eecda47c6d254f706e856206c3855720756684e0
Astel Thottankara <57724542+astelThottankara@users.noreply.github.com>
2020-05-12 10:20:30 +0530

    Update isolation-centres.java

--- Code Chef/May Challenge/isolation-centres.java
+++ Code Chef/May Challenge/isolation-centres.java
@@ -1,1 +1,35 @@
-//will be uploaded soon after the contest
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+	    int t = Integer.parseInt(br.readLine());
+        while(t-->0)
+	    {
+            String[] in = br.readLine().split(" ");
+            int n = Integer.parseInt(in[0]);
+            int q = Integer.parseInt(in[1]);
+            String s = br.readLine();
+            while(q-->0)
+            {
+                int c = Integer.parseInt(br.readLine());
+                long que=0;
+                int[] freq = new int[26];
+                for(int i=0;i<s.length();i++)
+                    freq[s.charAt(i)-'a']++;
+                for(int i=0;i<26;i++)
+                    freq[i] -= c;
+                for(int i=0;i<26;i++)
+                    if(freq[i]>0)
+                        que += freq[i];
+                pw.println(que);
+            }
+        }
+        pw.close();
+	}
+}
