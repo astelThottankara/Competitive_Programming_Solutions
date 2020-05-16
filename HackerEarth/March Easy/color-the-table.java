f00fe25307c8483a4f796d96251710a76a84a0ad
Astel Thottankara <57724542+astelThottankara@users.noreply.github.com>
2020-05-16 14:59:23 +0530

    Error corrected

--- HackerEarth/March Easy/color-the-table.java
+++ HackerEarth/March Easy/color-the-table.java
@@ -1,40 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class TestClass 
-{
-    private static long mod = (long)(1e9+7);
-    
-    static long expo(long base,long ex)
-    {
-        long result = 1;
-        while(ex>0)
-        {
-            if(ex%2==1)
-                result = (result * base)%mod;
-            base = (base*base)%mod;
-            ex /= 2;
-        }
-        return result;
-    }
-    
-    public static void main(String args[] ) throws Exception 
-    {
-        FastReader fr = new FastReader();
-        PrintWriter pw = new PrintWriter(System.out);
-        int t = fr.ni();
-        while(t-->0)
-        {
-            int n = fr.ni();
-            int m = fr.ni();
-            long one = expo(3,n+m);
-            long two = expo(2,((long)n*m));
-            long res = (one*two)%mod;
-            System.out.println(res);
-        }
-        pw.close();
-    }
-    static class FastReader
+class FastReader
     {
         BufferedReader br;
         StringTokenizer st;
@@ -88,5 +55,39 @@ class TestClass
             }
             return str;
         }
+    }
+    
+class TestClass 
+{
+    private static long mod = (long)(1e9+7);
+    
+    static long expo(long base,long ex)
+    {
+        long result = 1;
+        while(ex>0)
+        {
+            if(ex%2==1)
+                result = (result * base)%mod;
+            base = (base*base)%mod;
+            ex /= 2;
+        }
+        return result;
+    }
+    
+    public static void main(String args[] ) throws Exception 
+    {
+        Scanner s = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+        int t = s.nextInt();
+        while(t-->0)
+        {
+            int n = s.nextInt();
+            int m = s.nextInt();
+            long one = expo(3,n+m);
+            long two = expo(2,((long)n*m));
+            long res = (one*two)%mod;
+            System.out.println(res);
+        }
+        pw.close();
     }
 }
