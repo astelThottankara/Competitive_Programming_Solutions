a92af60474ac8a256ba7158a7eb32f4af3ff3944
Astel Thottankara <57724542+astelThottankara@users.noreply.github.com>
2020-05-23 14:04:09 +0530

    Create chef-chick.java

---
+++ Code Chef/January Cook-Off/chef-chick.java
@@ -0,0 +1,80 @@
+/*
+
+    author - astelThottankara
+
+*/
+
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer; 
+
+class CodeChef 
+{
+    public static void main(String args[] ) throws Exception 
+    {
+        FastReader fr = new FastReader();
+        PrintWriter pw = new PrintWriter(System.out);
+        int t = fr.ni();
+        while(t-->0)
+        {
+            int n = fr.ni();
+            int[] a = new int[n];
+            int min = Integer.MAX_VALUE;
+            for(int i=0;i<n;i++)
+                if((a[i] = fr.ni())<min)
+                    min = a[i];
+            pw.println(min);
+        }
+        pw.flush();
+        pw.close();
+    }
+}
+
+class FastReader
+{
+    BufferedReader br;
+    StringTokenizer st;
+        
+    public FastReader()
+    {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+        
+    String next()
+    {
+        while(st == null || !st.hasMoreTokens())
+        {
+            try
+            {
+                st = new StringTokenizer(br.readLine());
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+        
+    int ni()
+    {
+        return Integer.parseInt(next());
+    }
+       
+    long nl()
+    {
+        return Long.parseLong(next());
+    }
+        
+    double nd()
+    {
+        return Double.parseDouble(next());
+    }
+        
+    String ns()
+    {
+        return next();
+    }
+}
