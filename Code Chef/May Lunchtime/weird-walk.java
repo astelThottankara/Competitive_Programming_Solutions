fdaa9b2a42ecfdd30e11b29289307648135d8a3e
Astel Thottankara <57724542+astelThottankara@users.noreply.github.com>
2020-05-31 09:20:21 +0530

    Update weird-walk.java

--- Code Chef/May Lunchtime/weird-walk.java
+++ Code Chef/May Lunchtime/weird-walk.java
@@ -1,1 +1,90 @@
-//will be uploaded soon
+/*
+
+    author - astelThottankara
+
+*/
+
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer; 
+
+class CodeChef 
+{
+    public static void main(String args[] ) throws Exception 
+    {
+        FastReader fr = new FastReader();
+        PrintWriter pw = new PrintWriter(System.out);
+        int t = fr.ni();
+        while(t-->0)
+        {
+            int n = fr.ni();
+            int[] a = new int[n];
+            int[] b = new int[n];
+            long distA=0,distB=0;
+            for(int i=0;i<n;i++)
+                a[i] = fr.ni();
+            for(int i=0;i<n;i++)
+                b[i] = fr.ni();
+            long sum=0;
+            for(int i=0;i<n;i++)
+            {
+                distA += a[i];
+                distB += b[i];
+                if(a[i]==b[i] && distA==distB)
+                    sum += a[i];
+            }
+            pw.println(sum);
+        }
+        pw.flush();
+        pw.close();
+    }
+}
+
+class FastReader
+{
+    BufferedReader br;
+    StringTokenizer st;
+        
+    public FastReader()
+    {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+        
+    String next()
+    {
+        while(st == null || !st.hasMoreTokens())
+        {
+            try
+            {
+                st = new StringTokenizer(br.readLine());
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+        
+    int ni()
+    {
+        return Integer.parseInt(next());
+    }
+       
+    long nl()
+    {
+        return Long.parseLong(next());
+    }
+        
+    double nd()
+    {
+        return Double.parseDouble(next());
+    }
+        
+    String ns()
+    {
+        return next();
+    }
+}
