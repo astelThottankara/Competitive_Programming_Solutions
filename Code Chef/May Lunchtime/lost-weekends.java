8d70cdfc8f0ef47133c7555f8ab0cb82da66fe86
Astel Thottankara <57724542+astelThottankara@users.noreply.github.com>
2020-05-31 09:19:48 +0530

    Update lost-weekends.java

--- Code Chef/May Lunchtime/lost-weekends.java
+++ Code Chef/May Lunchtime/lost-weekends.java
@@ -1,1 +1,86 @@
-//will be uploaded soon
+/*
+
+    author - astelThottankara
+
+*/
+
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer; 
+
+class CodeChef 
+{
+    public static void main(String args[] ) throws Exception 
+    {
+        FastReader fr = new FastReader();
+        PrintWriter pw = new PrintWriter(System.out);
+        int t = fr.ni();
+        while(t-->0)
+        {
+            int[] a = new int[6];
+            for(int i=0;i<6;i++)
+                a[i] = fr.ni();
+            long sum=0;
+            for(int i=0;i<5;i++)
+            {
+                a[i] = 24-(a[i]*a[5]);
+                sum += a[i];
+            }
+            if(sum<0)
+                pw.println("Yes");
+            else
+                pw.println("No");
+        }
+        pw.flush();
+        pw.close();
+    }
+}
+
+class FastReader
+{
+    BufferedReader br;
+    StringTokenizer st;
+        
+    public FastReader()
+    {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+        
+    String next()
+    {
+        while(st == null || !st.hasMoreTokens())
+        {
+            try
+            {
+                st = new StringTokenizer(br.readLine());
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+        
+    int ni()
+    {
+        return Integer.parseInt(next());
+    }
+       
+    long nl()
+    {
+        return Long.parseLong(next());
+    }
+        
+    double nd()
+    {
+        return Double.parseDouble(next());
+    }
+        
+    String ns()
+    {
+        return next();
+    }
+}
