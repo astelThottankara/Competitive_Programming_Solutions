001aaacd6e2a3831056a7ec691339526ff0bdc62
Astel Thottankara <57724542+astonizer@users.noreply.github.com>
2020-11-01 13:57:50 +0530

    Add solution

---
+++ Code Chef/October Lunchtime/ANDOR.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPair(long x) {
+	cout << 0 << " " << x << endl;
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+	    long x;
+		cin >> x;
+		printPair(x);
+	}
+	return 0;
+}
